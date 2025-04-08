3ad908fa8ac0a0ceab9ed331d7ca1f776ef626db
gabrielCarvalhoGit <gabriel.carvalhogss@gmail.com>
2025-04-08 13:29:51 -0300

    feat: Finished product crud.

--- apps/products/api/views/product_views.py
+++ apps/products/api/views/product_views.py
@@ -1,14 +1,74 @@
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+
+from apps.products.services import ProductService
+from apps.products.api.serializers import ProductSerializer
 
 from apps.core.utils.permissions import UserPermission
 
 
 class ProductView(APIView):
     permission_classes = [IsAuthenticated, UserPermission]
+    serializer_class = ProductSerializer
 
     permission_app_label  = 'products'
     permission_model = 'product'
 
+    service = ProductService()
+
     def get(self, request):
-        pass
+        product_id = request.query_params.get('id', None)
+
+        if 'list' in request.GET:
+            products = self.service.get_all_products()
+            response = self.serializer_class(products, many=True)
+
+            return Response({'products': response.data}, status=status.HTTP_200_OK)
+        
+        if product_id:
+            product = self.service.get_product(product_id)
+            response = self.serializer_class(product)
+
+            return Response({'product': response.data}, status=status.HTTP_200_OK)
+        
+        return Response({'detail': 'Product ID is required.'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            product = self.service.create_product(**serializer.validated_data)
+            response = self.serializer_class(product)
+
+            return Response({'product': response.data}, status=status.HTTP_200_OK)
+        
+        return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request):
+        product_id = request.data.get('id')
+
+        if product_id:
+            product = self.service.get_product(product_id)
+            serializer = self.serializer_class(instance=product, data=request.data, partial=True)
+
+            if serializer.is_valid():
+                updated_product = self.service.update_product(product, **serializer.validated_data)
+                print(updated_product.name)
+                response = self.serializer_class(updated_product)
+
+                return Response({'product': response.data}, status=status.HTTP_200_OK)
+            
+            return Response({'detail': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
+        return Response({'detail': 'Product ID is required.'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request):
+        product_id = request.query_params.get('id', None)
+
+        if product_id:
+            self.service.delete_product(product_id)
+            return Response({'detail': 'Product deleted successfully.'}, status=status.HTTP_200_OK)
+        
+        return Response({'detail': 'Product ID is required.'}, status=status.HTTP_400_BAD_REQUEST)
--- apps/products/repositories/product_repository.py
+++ apps/products/repositories/product_repository.py
@@ -6,14 +6,17 @@ class ProductRepository:
     def exists_by_id(self, product_id: uuid.UUID) -> bool:
         return Product.objects.filter(id=product_id).exists()
     
-    def get_by_id(self, product_id: uuid.UUID) -> bool:
+    def get_by_id(self, product_id: uuid.UUID) -> Product:
         return Product.objects.get(id=product_id)
     
     def get_all(self) -> list[Product]:
         return Product.objects.all()
     
-    def create(self, produc_data: dict) -> Product:
-        Product.objects.create(**produc_data)
+    def create(self, product_data: dict) -> Product:
+        return Product.objects.create(**product_data)
 
     def delete(self, product_id: uuid.UUID) ->  None:
         Product.objects.filter(id=product_id).delete()
+    
+    def save(self, obj: Product) -> Product:
+        obj.save()

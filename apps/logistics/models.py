c6f20d238d1fdcfb169501b2411d8115bd491a91
gabrielCarvalhoGit <gabriel.carvalhogss@gmail.com>
2025-05-21 15:01:11 -0300

    bugfix: adjusting daily production logic to reuse unconsumed batches.

--- apps/logistics/models.py
+++ apps/logistics/models.py
@@ -6,11 +6,11 @@ from apps.products.models import Product
 
 class ProductionSchedule(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='production_order')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="production_schedule")
     production_date = models.DateField()
     batches = models.PositiveIntegerField()
+    packages = models.PositiveIntegerField()
 
     class Meta:
-        unique_together = ('product', 'order', 'production_date')
+        unique_together = ('product', 'production_date')
         ordering = ['production_date']

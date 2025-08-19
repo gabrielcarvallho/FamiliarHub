ed19611c1d4f0d23a81fe879276fb82d9f05fbe2
gabrielcarvallho <gabriellccarvalho.dev@gmail.com>
2025-08-19 08:00:58 -0300

    adjusting products model.

--- apps/products/models.py
+++ apps/products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     weight = models.DecimalField(max_digits=5, decimal_places=2)
 
